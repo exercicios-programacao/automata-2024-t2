eb66c68c83a9bc9f9a009532b819b8edcfadfa48
EduardoFSousaaa <164102632+EduardoFSousaaa@users.noreply.github.com>
2024-04-26 17:49:51 -0300

    Adicionado code t1
    Adicionado code t1

--- src/automata.py
+++ src/automata.py
@@ -1,7 +1,7 @@
 """Implementação de autômatos finitos."""
-
-
+from collections import namedtuple
 def load_automata(filename):
+    #print("load_automata")
     """
     Lê os dados de um autômato finito a partir de um arquivo.
 
@@ -33,22 +33,260 @@ def load_automata(filename):
     Caso o arquivo seja inválido uma exceção Exception é gerada.
 
     """
-
+    RegrasTransicao = namedtuple('RegrasTransicao',['origem' , 'símbolo', 'destino'])
     with open(filename, "rt") as arquivo:
+        texto = arquivo.readlines()
+        contador = 0
+        automata = {
+             'simbolos':[],
+             'estados':[],
+             'estadosFinais':[],
+             'NomeEstadoInicial':"",
+             'RegrasTransicao':[]
+        }
+        listaRegras = []
+        #print(texto)
+        for frase in texto:
+            #print(frase)
+            if contador == 0:
+                simbolos = frase.split()
+                automata['simbolos'].extend(simbolos)          
+            elif contador == 1:
+                estados = frase.split()
+                automata['estados'].extend(estados)
+            elif contador == 2:
+                estadosFinais = frase.split()
+                automata['estadosFinais'].extend(estadosFinais)
+            elif contador == 3:
+                NomeEstadoInicial = frase.split()
+                automata.update({"NomeEstadoInicial": NomeEstadoInicial[0]})
+            elif contador > 3:
+                palavras = frase.split()
+                if len(palavras) == 3:
+                    Regras=RegrasTransicao(palavras[0],palavras[1],palavras[2])
+                    #automata['RegrasTransicao'].extend(simbolos)
+                    listaRegras.append(Regras)
+                else :
+                    try:
+                        raise NameError('Regras Transisao Invalida')
+                    except NameError:
+                        print('Regras Transisao Invalida na sua quantidade de itens diferente de 3')
+                        raise
+                    break
+            contador= contador + 1
+            #print (contador)
+        
         # processa arquivo...
+      
+        else:
+            automata['RegrasTransicao'].extend(listaRegras)
         pass
-
-
+    #print(automata)
+    retStatusautomata = DescricaoautomataValida(automata)
+    words = ["ababa","cc"]
+    retprocess = process(automata,words)
+    print("Lista Palavras e resultados")
+    print(retprocess)
 def process(automata, words):
     """
     Processa a lista de palavras e retora o resultado.
     
     Os resultados válidos são ACEITA, REJEITA, INVALIDA.
     """
-
+    caminhoDoAutonomo=""
+    EstadoInicial =  str(automata.get('NomeEstadoInicial'))
+    estadosFinais =  str(automata.get('estadosFinais'))
+    simbolos = str(automata.get('simbolos'))
+    listaRegras =  automata.get('RegrasTransicao')
+    #for regras in listaRegras:
+        #print("\torigem: "+regras[0]+" símbolo: "+regras[1]+" destino: "+regras[2])
+    resultado = "OK"
+    contador = 0;
+    palavraAserMontada=""
+    DictWord={}
     for word in words:
         # tenta reconhecer `word`
+        retTuple = VerificaPalavra(word,simbolos)
+        #print(retTuple[1]+" retet")
+        if(retTuple[1]=='VALIDA'):
+            if str(word)!="":
+                for caractere in str(word):
+                #verifica se é posição do estado inicial
+                    if contador == 0:
+                        for regras in listaRegras:
+                            #se origem for igual estado inicial
+                            if regras[0] == EstadoInicial:
+                                #se simbolo for igual caracter
+                                if regras[1] == caractere:
+                                    caminhoDoAutonomo = caminhoDoAutonomo+ "Orig: " + regras[0] + " Simb: " + regras[1] + " Destino: " + regras[2]+ "\n"
+                                    ProximaOrigem = regras[2]
+                                    palavraAserMontada += caractere
+                                    break
+                        else:
+                            ProximaOrigem=0
+                            resultado="REJEITA"
+                            break
+                    else:
+                        for regras in listaRegras:
+                            #se origem for igual estado inicial
+                            if regras[0] == ProximaOrigem:
+                                #se simbolo for igual caracter
+                                if regras[1] == caractere:
+                                    caminhoDoAutonomo = caminhoDoAutonomo+ "Orig: "+ regras[0]+  " Simb: " + regras[1] + " Destino:"+regras[2] + "\n"
+                                    ProximaOrigem = regras[2]
+                                    palavraAserMontada += caractere
+                                    break
+                        else:
+                            ProximaOrigem=0
+                            resultado="REJEITA"
+                            break
+                    contador+=1
+        
+            if palavraAserMontada == str(word):
+                if ProximaOrigem in estadosFinais:
+                    resultado = "ACEITA"
+                else:
+                    resultado = "REJEITA"
+            else:
+                resultado = "REJEITA"
+            #print("\n")
+            #print("CaminhoAutonomo: " + caminhoDoAutonomo)
+            #print(palavraAserMontada)
+            #print(resultado)    
+            DictWord[word] = resultado
+        else:
+            DictWord[word] = 'INVALIDA'
+        pass
+    return DictWord
+#Verificar se uma palavra válida, ou seja, se todos os símbolos da palavra fazem parte do alfabeto da lingugaem
+def VerificaPalavra(palavra,simbolos):
+    for caractere in str(palavra):
+        for simbolo in simbolos:
+            if simbolo in caractere:
+                #print("SIM")
+                resultado = 'VALIDA'
+                break
+        else:
+            #print("NAO")
+            resultado = 'INVALIDA'
+            break
+    return tuple((palavra,resultado))
 
-
+def DescricaoautomataValida(automata):
+    print("DescricaoautomataValida")
+    #print(automata)
+    if (automata.get('simbolos')!=''
+        and len(automata.get('estados'))>0
+        and len(automata.get('estadosFinais'))>0
+        and automata.get('NomeEstadoInicial')!=''
+        and len(automata.get('RegrasTransicao'))>0):
+        #verifica se os estados finais é valido com algum estado
+        #verifica estados finais
+        cont=0
+        for esF in automata.get('estadosFinais'):
+            for es in automata.get('estados'):
+                if(esF == es):
+                    cont+=1
+                    Statusautomata='VALIDO'
+                    break
+        #verifica se a mesma quantidade de estados finais é valido com algum estado
+        #verifica estados finais
+        if(len(automata.get('estadosFinais')) == cont):
+            Statusautomata='VALIDO'
+        else:
+            Statusautomata='INVALIDO'
+            try:
+                raise NameError('estadosFinais')
+            except NameError:
+                print('os estadosFinais Não São todos validos')
+                raise
+        #Zera a variavel para usar novamente no proximo teste
+        Statusautomata=""
+        #verifica NomeEstadoInicial
+        print(automata.get('NomeEstadoInicial'))
+        for es in automata.get('estados'):
+            #print(es)
+            #print((automata.get('NomeEstadoInicial') == es))
+            if(automata.get('NomeEstadoInicial') == es):
+                Statusautomata='VALIDO'
+                break
+            else:
+                print(Statusautomata)
+                if not Statusautomata == 'VALIDO':
+                    Statusautomata='INVALIDO'
+                    try:
+                        raise NameError('NomeEstadoInicial')
+                    except NameError:
+                        print('o Nome do Estado Inicial Não é validos')
+                        raise
+                    break
+                    return Statusautomata
+        RegrasTransicao = namedtuple('RegrasTransicao',['origem' , 'símbolo', 'destino'])
+        regras=automata.get('RegrasTransicao')
+        #print(regras)
+        ListDistintaEstados = []
+        ListDistintaSimbolos = []
+        for r in regras:
+            print(r[0]+" - " + r[1] +" - "+r[2])
+            if r[0] not in ListDistintaEstados:
+                ListDistintaEstados.append(r[0])
+            if r[1] not in ListDistintaSimbolos:
+                ListDistintaSimbolos.append(r[1])
+            if r[2] not in ListDistintaEstados:
+                ListDistintaEstados.append(r[2])
+        #print(ListDistintaEstados)
+        #print(ListDistintaSimbolos)
+        #print(automata.get('simbolos'))
+        cont=0
+        for esL in ListDistintaEstados:
+            for es in automata.get('estados'):
+                if(esL in es):
+                    #se existe o estado nas regras de transição e na lista de estados soma-se 1
+                    cont+=1
+                    Statusautomata='VALIDO'
+                    break;
+        #Verifica-se se para cada estado diferente em nossa regra de transisão existe um estado com mesmo nome.
+        #valida a contagem se todos tiveram um correspondente
+        if(len(ListDistintaEstados) == cont):
+            Statusautomata='VALIDO'
+        else:
+            try:
+                raise NameError('EstadosDaRegraTransisao')
+            except NameError:
+                print('Os Estados da regra de transição Nem todos São validos')
+                raise
+            Statusautomata='INVALIDO'
+            return Statusautomata
+        cont=0
+        for siL in ListDistintaSimbolos:
+            for si in automata.get('simbolos'):
+                if(siL in si):
+                    #se existe o simbolo nas regras de transição e na lista de simbolos soma-se 1
+                    cont+=1
+                    Statusautomata='VALIDO'
+                    break;
+        #Verifica-se se para cada simbolo diferente em nossa regra de transisão existe um simbolo com mesmo nome.
+        #valida a contagem se todos tiveram um correspondente
+        if(len(ListDistintaSimbolos) == cont):
+            Statusautomata='VALIDO'
+        else:
+            Statusautomata='INVALIDO'
+            try:
+                raise NameError('SimbolosRegraTransisao')
+            except NameError:
+                print('O Simbolo da regra de transição Nao é valido')
+                raise
+            return Statusautomata
+        print(Statusautomata)
+        print("VALIDO")
+    else:
+        Statusautomata='INVALIDO'
+        return Statusautomata
 def convert_to_dfa(automata):
     """Converte um NFA num DFA."""
+    
+def main():
+    #filename = ".\\Testes\\05-invalido.txt"
+    filename = "teste.txt"
+    load_automata(filename)
+main()
