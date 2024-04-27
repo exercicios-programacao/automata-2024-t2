75f9f0d2e09033d430151524de6c874e34717bcb
eduardassalim <167265171+eduardassalim@users.noreply.github.com>
2024-04-27 15:04:39 -0300

    fazer t2 a partir do t1

--- src/automata.py
+++ src/automata.py
@@ -1,54 +1,57 @@
-"""Implementação de autômatos finitos."""
+#lista das variáveis
+class Automato:
+    def __init__(self, estados, alfabeto, estadoInicial, estadosFinais, nodos):
+        self.estados = estados
+        self.alfabeto = alfabeto
+        self.estadoInicial = estadoInicial
+        self.estadosFinais = estadosFinais
+        self.nodos = nodos
 
+    def entradaValida(self, entrada):
+        estadoAtual = self.estadoIncial # estado atual recebe estado inicial
+        for simbolo in entrada: 
+            if(estadoAtual, simbolo) not in self.nodos:
+                return False
+        estadoAtual = self.nodos[(estadoAtual, simbolo)][0]
+
+        return estadoAtual in self.estadosFinais #faz a verificação do estado, se o estado final é um estado de aceitação ou não
 
 def load_automata(filename):
-    """
-    Lê os dados de um autômato finito a partir de um arquivo.
+    with open("01-simples.txt", "rt") as arquivo: #abre o arquivo para fazer a leitura de todas a linhas do arquivo
+        linhas = arquivo.readlines()
 
-    A estsrutura do arquivo deve ser:
+# aqui faz um verificação se o arquivo tem ao menos 5 linhas, caso não tenha, é um arquivo inválido
+    if len(linhas) < 5:
+        raise ValueError("arquivo inválido")
 
-    <lista de símbolos do alfabeto, separados por espaço (' ')>
-    <lista de nomes de estados>
-    <lista de nomes de estados finais>
-    <nome do estado inicial>
-    <lista de regras de transição, com "origem símbolo destino">
+#aqui faz a extração das linhas do arquivo
+    estadoInicial = linhas[0]
+    alfabeto = linhas[1].split(" ")
+    estados = linhas[2].split(" ")
+    estadosFinais = linhas[3].split(" ")
+    #nodos = linhas[4:]
+    nodos = {}
 
-    Um exemplo de arquivo válido é:
+    for linha in linhas[4:]: #para cada linha de transição depois da 4 linha separa as linhas
+        origem, simbolo, destino = linha.split("")
+        if (origem, simbolo) not in nodos:
+            nodos[(origem, simbolo)] = []
+        nodos[(origem, simbolo)].append(destino)
 
-    ```
-    a b
-    q0 q1 q2 q3
-    q0 q3
-    q0
-    q0 a q1
-    q0 b q2
-    q1 a q0
-    q1 b q3
-    q2 a q3
-    q2 b q0
-    q3 a q1
-    q3 b q2
-    ```
+    #aqui cria um objeto Automato com os dados lidos
+    automato = Automato(estadoInicial, alfabeto, estados, estadosFinais, nodos)
+    return automato
 
-    Caso o arquivo seja inválido uma exceção Exception é gerada.
+def main():
+    file = input("Digite o nome do arquivo que contém o autômato: ")
+    try:
+        automato = load_automata(file)
+        if automato.entradaValida(file):
+            print("ACEITA")
+        else:
+            print("REJEITA")
+    except Exception as e:
+        print("Arquivo Inválido")
 
-    """
-
-    with open(filename, "rt") as arquivo:
-        # processa arquivo...
-        pass
-
-
-def process(automata, words):
-    """
-    Processa a lista de palavras e retora o resultado.
-    
-    Os resultados válidos são ACEITA, REJEITA, INVALIDA.
-    """
-
-    for word in words:
-        # tenta reconhecer `word`
-
-
-def convert_to_dfa(automata):
-    """Converte um NFA num DFA."""
+if __name__ == "__main__":
+    main()
