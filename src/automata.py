fea9b260b1c95b15ea0ca624f71c0a65748daea3
eduardassalim <167265171+eduardassalim@users.noreply.github.com>
2024-04-28 17:36:37 -0300

    atualizando

--- src/automata.py
+++ src/automata.py
@@ -1,57 +1,53 @@
-#lista das variáveis
-class Automato:
-    def __init__(self, estados, alfabeto, estadoInicial, estadosFinais, nodos):
-        self.estados = estados
-        self.alfabeto = alfabeto
-        self.estadoInicial = estadoInicial
-        self.estadosFinais = estadosFinais
-        self.nodos = nodos
+def load_automaton(filename):
 
-    def entradaValida(self, entrada):
-        estadoAtual = self.estadoIncial # estado atual recebe estado inicial
-        for simbolo in entrada: 
-            if(estadoAtual, simbolo) not in self.nodos:
-                return False
-        estadoAtual = self.nodos[(estadoAtual, simbolo)][0]
-
-        return estadoAtual in self.estadosFinais #faz a verificação do estado, se o estado final é um estado de aceitação ou não
-
-def load_automata(filename):
-    with open("01-simples.txt", "rt") as arquivo: #abre o arquivo para fazer a leitura de todas a linhas do arquivo
-        linhas = arquivo.readlines()
-
-# aqui faz um verificação se o arquivo tem ao menos 5 linhas, caso não tenha, é um arquivo inválido
-    if len(linhas) < 5:
-        raise ValueError("arquivo inválido")
-
-#aqui faz a extração das linhas do arquivo
-    estadoInicial = linhas[0]
-    alfabeto = linhas[1].split(" ")
-    estados = linhas[2].split(" ")
-    estadosFinais = linhas[3].split(" ")
-    #nodos = linhas[4:]
-    nodos = {}
-
-    for linha in linhas[4:]: #para cada linha de transição depois da 4 linha separa as linhas
-        origem, simbolo, destino = linha.split("")
-        if (origem, simbolo) not in nodos:
-            nodos[(origem, simbolo)] = []
-        nodos[(origem, simbolo)].append(destino)
-
-    #aqui cria um objeto Automato com os dados lidos
-    automato = Automato(estadoInicial, alfabeto, estados, estadosFinais, nodos)
-    return automato
-
-def main():
-    file = input("Digite o nome do arquivo que contém o autômato: ")
+    #aqui abre o arquivo e lê as linhas, se não houver transições vai apresentar mensagem de erro
     try:
-        automato = load_automata(file)
-        if automato.entradaValida(file):
-            print("ACEITA")
-        else:
-            print("REJEITA")
-    except Exception as e:
-        print("Arquivo Inválido")
+        with open(filename, 'r') as file:
+            linha = [line.strip() for line in file.readlines()]
 
-if __name__ == "__main__":
-    main()
+        #aqui retiro as informações do arquivo 
+        alfabeto = linha[0].split(" ")
+        estados = linha[1].split(" ")
+        estadoFinais = linha[2].split(" ")
+        estadoInicial = linha[3].strip(" ")
+        nodos = linha[4:]
+
+        #cria a função da transição entre os estados
+        transição = {estado: {} for estado in estados}
+
+        for nodo in nodos:
+            origem, simbolo, destino = nodo.split()
+            if simbolo in transição[origem]:
+                raise Exception("transição não encontrada")
+            transição[origem][simbolo] = destino
+
+        return estados, alfabeto, transição, estadoInicial, set(estadoFinais)
+   
+    except Exception as e:
+        raise Exception (e)
+
+def process(automato, words):
+    estados, alfabeto, delta, estadoInicial, estadosFinais = automato
+    
+    processos = {}
+    
+    for word in words:
+        estadoAtual = estadoInicial
+        falso = False
+        
+        for simbolo in word:
+            if simbolo not in alfabeto:
+                estadoAtual[word] = "INVÁLIDA"
+                falso = True
+                break
+            try:
+                estadoAtual = delta[estadoAtual][simbolo]
+            except Exception:
+                estadoAtual[word] = "REJEITA"
+                falso = True
+                break
+        
+        if not falso:
+            estadoAtual[word] = "ACEITA" if estadoAtual in estadosFinais else "REJEITA"
+    
+    return processos
