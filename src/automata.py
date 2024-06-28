d2aeec72e6f80188cf2ea6ef4da7ab40c804a7f5
eduardassalim <167265171+eduardassalim@users.noreply.github.com>
2024-06-28 09:54:51 -0300

    Update automata.py
    copiando t1 para t2

--- src/automata.py
+++ src/automata.py
@@ -1,57 +1,95 @@
+"""Aqui vou implementar dfa."""
+
+
+class ErroException(Exception):
+    """Aqui crio uma exceção personalizada.
+
+    Args:
+        mensagem (str): descrição do erro encontrado.
+    """
+
+    def __init__(self, mensagem):
+        """Aqui inicializa a exceção.
+
+        Args:
+            mensagem (str): Mensagem do erro.
+        """
+        self.mensagem = mensagem
+        super().__init__(self.mensagem)
+
+
 def load_automata(filename: str):
-	try:
-		with open(filename, encoding="utf-8") as arquivo:
-			linhas = arquivo.readlines()
-			
-			transicoes = {}
+    """Aqui carrega um autômato a partir de um arquivo."""
+    try:
+        with open(filename, encoding='utf-8') as arquivo:
+            linhas = arquivo.readlines()
 
-			if len(linhas) < 5:
-				raise ValueError("Arquivo não contém suficientes linhas para representar um autômato")
+            if len(linhas) < 5:
+                raise ErroException("Arquivo não é autômato.")
 
-			alfabeto = linhas[0].strip().split(" ")
-			estados = linhas[1].strip().split(" ")
-			estadosFinais = linhas[2].strip().split(" ")
-			estadoIncial = linhas[3].strip(" ")
+            alfabeto = linhas[0].strip().split()
+            estados = linhas[1].strip().split()
+            estados_finais = linhas[2].strip().split()
+            estado_inicial = linhas[3].strip()
+            transicao = [linha.strip().split() for linha in linhas[4:]]
 
-			for linha in linhas[4:]:
-				transicao = linha.strip().split(" ")
+            transicoes = {}
 
-				if len(transicao) < 3 or transicao[0] not in estados or transicao[1] not in alfabeto or transicao[2] not in estados:
-					raise ValueError("Transição não determinística não encontrada")
-				print(f"{transicao} é o correto.")
+            for estado in estados:
+                transicoes[transicoes] = []
+                for simbolo in alfabeto:
+                    transicoes[estado][simbolo] = None
 
-			return alfabeto, estados, estadosFinais, estadoIncial, transicoes
+            for origem, simbolo, destino in transicao:
+                if origem in estados and simbolo in alfabeto and destino in estados:
+                    transicao[origem][simbolo] = destino
+                else:
+                    raise ErroException("Transição inválida.")
 
-	except FileNotFoundError as e:
-		raise ValueError(f"Arquivo {filename} não encontrado") from e
+            for estado in estados_finais:
+                if estado not in estados:
+                    raise ErroException("Estado final não encontrado.")
+
+            if estado_inicial not in estados:
+                raise ErroException("Estado inicial não encontrado")
+
+            automata = alfabeto, estados, transicao, estado_inicial, estados_finais
+            return automata
+
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"Arquivo {filename} não encontrado.") from e
 
 
-def process(automata, word):
-	alfabeto, estados, estadosFinais, estadoIncial, transicoes = automata
+def process(automata, words):
+    """Aqui processa lista de palavras."""
+    alfabeto, estados_finais, estado_inicial, transicoes = automata
 
-	verifica = []
-	try:
-		for words in word:
-			palavra = tuple(words)
+    verifica = {}
+    try:
+        for word in words:
+            estado_atual = estado_inicial
+            verificacao = True
 
-			for simbolo in palavra:
-				if simbolo not in alfabeto:
-					verifica[words] = "INVÁLIDA"
-					break
-			else:
-				estadoAtual = estadoIncial
-				for simbolo in palavra:
-					if simbolo in transicoes.get[estadoAtual, []]:
-						estadoAtual = transicoes[estadoAtual][simbolo]
-					else:
-						verifica[words] = "REJEITA"
-						break
-				else:
-					if estadoAtual in estadosFinais:
-						verifica[words] = "ACEITA"
-					else:
-						verifica[words] = "REJEITA"
-	except Exception as e:
-		raise ValueError(f"Erro ao processar palavra {word}: {e}") from e
-		
-	return verifica
+            for simbolo in word:
+                if simbolo not in alfabeto:
+                    verifica[word] = "INVÁLIDA"
+                    verificacao = False
+                    break
+
+                estado_atual = transicoes[estado_atual].get(simbolo)
+
+                if estado_atual is None:
+                    verifica[word] = "REJEITA"
+                    verificacao = False
+                    break
+
+                if verificacao:
+                    if estado_atual in estados_finais:
+                        verifica[word] = "ACEITA"
+                    else:
+                        verifica[word] = "REJEITA"
+
+    except Exception as e:
+        raise ErroException(f"Erro ao processar palavra '{word}': {e}.") from e
+
+    return verifica
